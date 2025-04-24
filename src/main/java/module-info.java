12c8e57cdb3ad778145dae092eaa1f9dd83db99c
Kasra <kasra.sh1385@gmail.com>
2025-04-24 18:13:21 +0330

    update module-info.java and added StartMenu.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module module_name {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires com.google.gson;
 
     exports views;
     opens views to javafx.fxml;
